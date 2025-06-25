4d0f1651c034b011e02c4c79ed64e492cda916bc
MattiaNatella <mattianatella@gmail.com>
2025-06-25 13:31:21 +0200

    Fix database configuration and add migration for workouts table

--- database/migrations/2025_06_25_094539_create_workouts_table.php
+++ database/migrations/2025_06_25_094539_create_workouts_table.php
@@ -10,7 +10,7 @@ return new class extends Migration {
      */
     public function up(): void
     {
-        Schema::create('workout', function (Blueprint $table) {
+        Schema::create('workouts', function (Blueprint $table) {
             $table->id();
 
             $table->foreignId('athlete_id')->constrained();
