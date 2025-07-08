47fa1c519654f4c7ad68fa7f3b4331da21bc352c
MattiaNatella <mattianatella@gmail.com>
2025-07-08 18:50:28 +0200

    Add ExerciseWorkoutTableSeeder to DatabaseSeeder for improved data seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             ExerciseTableSeeder::class,
             AthleteTableSeeder::class,
-            WorkoutTableSeeder::class
+            WorkoutTableSeeder::class,
+            ExerciseWorkoutTableSeeder::class
         ]);
     }
 }
