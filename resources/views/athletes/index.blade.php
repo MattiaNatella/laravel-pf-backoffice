78938bf8e97d15de39d3cbcc6fce121e9b513924
MattiaNatella <mattianatella@gmail.com>
2025-07-12 18:39:16 +0200

    feat: Refactor athlete card layout and improve styling for better readability

--- resources/views/athletes/index.blade.php
+++ resources/views/athletes/index.blade.php
@@ -6,34 +6,32 @@
         <a class="btn btn-info my-2" href="{{ route('admin.athletes.create') }}">Aggiungi un nuovo Atleta</a>
     </div>
 
-    <div class="container d-flex flex-wrap gap-2 justify-content-center my-3">
-
+    <div class="container d-flex flex-wrap justify-content-center gap-3">
         @foreach ($athletes as $athlete)
-
-            <div class="card mb-3" style="max-width: 540px; overflow: hidden;">
+            <div class="card mb-3">
                 <div class="row g-0">
-                    <div class="col-md-4 p-0">
-                        <img src=" {{ ($athlete->image) ? asset('storage/' . $athlete->image) : "https://placehold.co/256x305/png" }}"
+                    <div class="col-md-4">
+                        <img src="{{ ($athlete->image) ? asset('storage/' . $athlete->image) : "https://placehold.co/256x305/png" }}"
                             class="rounded-start w-100 copertina-atleta" alt="copertina-atleta">
                     </div>
                     <div class="col-md-8">
-                        <div class="card-body px-1">
+                        <div class="card-body">
                             <h5 class="card-title">{{$athlete->name}} {{ $athlete->surname }}</h5>
-                            <p class="card-text"><strong>Altezza:</strong> {{ $athlete->height_cm }}cm - <strong>Peso
-                                    iniziale:</strong> {{ $athlete->initial_weight }}kg</p>
-                            <a class="btn btn-info text-white" href="{{ route('admin.athletes.show', $athlete) }}">Dettagli</a>
-                            <a class="btn btn-warning text-white"
-                                href="{{ route('admin.athletes.edit', $athlete) }}">Modifica</a>
-                            <!-- Button trigger modal -->
-                            <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                                Elimina
-                            </button>
+                            <p class="card-text"><small><strong>Altezza:</strong>
+                                    {{ $athlete->height_cm }}cm<br><strong>Peso:</strong>
+                                    {{ $athlete->initial_weight }}kg</small></p>
+                            <div class="d-flex gap-1 flex-wrap">
+                                <a class="btn btn-info text-white"
+                                    href="{{ route('admin.athletes.show', $athlete) }}">Dettagli</a>
+                                <a class="btn btn-warning text-white"
+                                    href="{{ route('admin.athletes.edit', $athlete) }}">Modifica</a>
+                                <button type="button" class="btn btn-danger" data-bs-toggle="modal"
+                                    data-bs-target="#exampleModal">Elimina</button>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-
-
         @endforeach
     </div>
 @endsection
@@ -59,7 +57,6 @@
                     @csrf
                     @method('DELETE')
                     <button type="submit" class="btn btn-danger">Elimina</button>
-
                 </form>
             </div>
         </div>
